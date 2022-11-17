ace7644991b82f05a46c7ff8e90c874c1dbeed42
pujarishilpa <shilpapujari286@gmail.com>
2022-11-17 19:53:14 +0530

    Added Even Or Odd Program

---
+++ NaturalNumberProgram.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+         if(n > 1) {
+         cout<<"It is a Natural Number!";
+    }
+         else {
+        cout<<"Not a Natural Number!";
+    }
+}
